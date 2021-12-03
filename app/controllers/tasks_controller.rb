c31adcfed6c89fff6d170f7534199ea2c0990a18
yuki525 <entio777@gmail.com>
2021-12-03 08:17:53 +0000

    view修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -32,6 +32,6 @@ class TasksController < ApplicationController
 
   private
     def task_params
-      params.require(:task).permit(:title)
+      params.require(:task).permit(:title, :body)
     end
 end
